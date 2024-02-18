ddc593f48c390622ee92928a2ebea6459af3a8b8
SpencerPresley <spencerpresley96@gmail.com>
2024-02-18 14:55:04 -0500

    Work towards a custom Neural Net. Testing with a small subset of arxiv.

---
+++ PythonCode/NeuralNet/testing/dataset_testing.py
@@ -0,0 +1,10 @@
+import tensorflow_datasets as tfds
+
+# Load hugginface arxiv data set: 
+# https://huggingface.co/datasets/arxiv_dataset
+# https://www.tensorflow.org/datasets/community_catalog/huggingface/arxiv_dataset
+ds = tfds.load('huggingface:arxiv_dataset')
+
+# Convert first batch to numpy and print
+for example in ds.take(1).as_numpy_iterator():
+    print(example)
