4757d33980337b17e64bfa74ddbaa550af9c7a1d
SpencerPresley <spencerpresley96@gmail.com>
2024-02-27 14:14:46 -0500

    Moved from a dictionary structure to Dataclass structure for the wos_classification, utilities, and faculty_set_postprocessor pipeline.

--- PythonCode/Utilities/My_Data_Classes.py
+++ PythonCode/Utilities/My_Data_Classes.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 from typing import Set
 
 @dataclass
@@ -9,4 +9,18 @@ class CategoryInfo:
     faculty: Set[str] = field(default_factory=set)
     departments: Set[str] = field(default_factory=set)
     article_set: Set[str] = field(default_factory=set)
+    
+    def to_dict(self) -> dict:
+        # Utilize asdict utility from dataclasses, then change sets to lists
+        data_dict = asdict(self)
+        
+        # Exclude 'files' from the dictionary
+        if 'files' in data_dict:
+            del data_dict['files']
+        
+        # Convert sets to lists for JSON serialization
+        for key, value in data_dict.items():
+            if isinstance(value, Set):
+                data_dict[key] = list(value)
+        return data_dict
 
