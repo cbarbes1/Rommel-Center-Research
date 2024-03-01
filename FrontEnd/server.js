2a83849b04eb518a623459dd9afa4b71609df7ad
Jude <judemaggitti123@gmail.com>
2024-02-29 22:08:08 -0500

    Basic Server Implementation

--- FrontEnd/server.js
+++ FrontEnd/server.js
@@ -1,25 +1,29 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var path = require('path');
 var express = require('express');
 var app = express();
 
-//app.use(express.static('style'));
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
+  var extname = path.extname(filename);
+  var contentType = 'text/html';
 
- // if (!fs.statSync(filename).isDirectory()) {
+  if (extname === '.css') {
+    contentType = 'text/css';
+  }
+
   fs.readFile(filename, function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
       return res.end("404 Not Found");
     } 
     
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, {'Content-Type': contentType});
     res.write(data);
     return res.end();
   });
-//}
 }).listen(6969);
