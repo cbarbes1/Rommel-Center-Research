30e389195bca5cef6b50e3c287d4c7fe3b2fbaa0
SpencerPresley <spencerpresley96@gmail.com>
2024-03-06 21:20:03 -0500

    Reformatted code using Python Black

--- PythonCode/Deprecated/deprecated_functions.py
+++ PythonCode/Deprecated/deprecated_functions.py
@@ -8,14 +8,15 @@ This is just a dump of methods and other things no longer needed but that I want
 
 import re
 
+
 class DeprecatedFunctions:
     def get_author(self, entry):
         """
         Extracts the author of the article from the entry, considering the format "last name, first name middle initial."
-        
+
         Arguments:
             entry (str): The text of the article entry from which to extract the author.
-        
+
         Returns:
             tuple: A tuple where the first element is a boolean indicating if the author was found,
                 and the second element is the author itself if found, or None otherwise.
@@ -24,14 +25,14 @@ class DeprecatedFunctions:
         if match:
             return True, match.group(1).strip()
         return False, None
-    
+
     def get_title(self, entry):
         """
         Extracts the title of the article from the entry.
-        
+
         Arguments:
             entry (str): The text of the article metadata entry from which to extract the title.
-        
+
         Returns:
             tuple: A tuple where the first element is a boolean indicating if the title was found,
                 and the second element is the title itself if found, or None otherwise.
@@ -40,14 +41,14 @@ class DeprecatedFunctions:
         if match:
             return True, match.group(1).strip()
         return False, None
-    
+
     def get_abstract(self, entry):
         """
         Extracts the abstract of the article from the entry.
-        
+
         Arguments:
             entry (str): The text of the article entry from which to extract the abstract.
-        
+
         Returns:
             tuple: A tuple where the first element is a boolean indicating if the abstract was found,
                 and the second element is the abstract itself if found, or None otherwise.
--- PythonCode/Utilities/faculty_department_manager.py
+++ PythonCode/Utilities/faculty_department_manager.py
@@ -1,9 +1,11 @@
 import warnings
 import time
+
+
 class FacultyDepartmentManager:
     def __init__(self, category_processor):
         self.category_processor = category_processor
-    
+
     def update_faculty_set(self, categories, faculty_members):
         for category in categories:
             if category in self.category_processor.category_counts:
@@ -11,13 +13,14 @@ class FacultyDepartmentManager:
                 for faculty_member in faculty_members:
                     category_info.faculty.add(faculty_member)
             else:
-                warnings.warn(f"Warning: Category {category} not found in category_counts. Continuing to next.")
-                
+                warnings.warn(
+                    f"Warning: Category {category} not found in category_counts. Continuing to next."
+                )
+
     def update_department_set_2(self, categories, department_info):
         if department_info == "Unknown":
             return
-        
-        
+
         else:
             department_members = department_info
             for category in categories:
@@ -29,19 +32,24 @@ class FacultyDepartmentManager:
                     elif isinstance(department_members, str):
                         category_info.departments.add(department_members)
                     else:
-                        warnings.warn(f"Unexpected department_members type: {type(department_members)}")
+                        warnings.warn(
+                            f"Unexpected department_members type: {type(department_members)}"
+                        )
                 else:
-                    warnings.warn(f"WARNING: Category {category} not found in category_counts. Continuing to next category.")
+                    warnings.warn(
+                        f"WARNING: Category {category} not found in category_counts. Continuing to next category."
+                    )
+
     def update_faculty_count(self):
         for category, category_info in self.category_processor.category_counts.items():
             category_info.faculty_count = len(category_info.faculty)
-    
+
     def update_department_count(self):
         for category, category_info in self.category_processor.category_counts.items():
             category_info.department_count = len(category_info.departments)
-            
+
     def update_article_counts(self, categories_dict):
         """Returns total article count which is = to length of the files set"""
         for _, info in categories_dict.items():
             info.article_count = len(info.files)
-        return categories_dict 
+        return categories_dict
