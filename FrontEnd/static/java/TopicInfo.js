d2104cb6e398fb19f8a2399e157c1bacb56ebced
Jude <judemaggitti123@gmail.com>
2024-03-10 09:52:52 -0400

    Topic Page now dynamically shows data

--- FrontEnd/static/java/TopicInfo.js
+++ FrontEnd/static/java/TopicInfo.js
@@ -11,16 +11,16 @@ function fetchTopic(key) {
             <li>
                 <p class="large-font">Plant_Sciences</p>
                 <ul>
-                    <li><p class="faculty_count">Faculty Count: ${abArr.Plant_Sciences.faculty_count}</p></li>
-                    <li><p class="department_count">Department Count: ${abArr.Plant_Sciences.department_count}</p></li>
-                    <li><p class="article_count">Article Count: ${abArr.Plant_Sciences.article_count}</p></li>
+                    <li><p class="faculty_count">Faculty Count: ${abArr[key].faculty_count}</p></li>
+                    <li><p class="department_count">Department Count: ${abArr[key].department_count}</p></li>
+                    <li><p class="article_count">Article Count: ${abArr[key].article_count}</p></li>
                     <li>
                         <p class="medium font">Faculty List:</p>
-                        <ul>${abArr.Plant_Sciences.faculty_set.map(item => `<li><p class="medium font">${item}</p></li>`).join('')}</ul>
+                        <ul>${abArr[key].faculty_set.map(item => `<li><p class="medium font">${item}</p></li>`).join('')}</ul>
                     </li>
                     <li>
                         <p class="medium font">Department List:</p>
-                        <ul>${abArr.Plant_Sciences.department_set.map(item => `<li><p class="medium font">${item}</p></li>`).join('')}</ul>
+                        <ul>${abArr[key].department_set.map(item => `<li><p class="medium font">${item}</p></li>`).join('')}</ul>
                     </li>
                 </ul>
             </li>
@@ -34,5 +34,5 @@ function fetchTopic(key) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    fetchTopic(0);
+    fetchTopic("Plant_Sciences");
 });
