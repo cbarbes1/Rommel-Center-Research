d28ad6196d556f3f3be33ea822a4742c704770bb
Jude Maggitti <judemaggitti123@gmail.com>
2024-03-11 18:43:19 -0400

    Merging all branchs, - Spencer

---
+++ FrontEnd/static/java/ArticleListSearch.js
@@ -0,0 +1,26 @@
+function fetchArticleListData() {
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function () {
+        const abArr = JSON.parse(this.responseText);
+
+        const article_List = document.getElementById("article_List");
+
+        article_List.innerHTML = '';
+
+        Object.keys(abArr).forEach(function (item) {
+            const articleListItem = document.createElement("ul");
+            articleListItem.innerHTML = `
+            <li>
+             <a href = "/html/Article.html?key=${encodeURIComponent(item)}" ><p class = "medium font">${item}</p></a>
+           </form>
+         </li>
+            `;
+            article_List.appendChild(articleListItem);
+        });
+    };
+    xmlhttp.open("GET", "/static/json/AuthorSample.json", true);
+    xmlhttp.send();
+}
+document.addEventListener("DOMContentLoaded", function () {
+    fetchArticleListData();
+});
---
+++ FrontEnd/static/java/FacultyListSearch.js
@@ -0,0 +1,26 @@
+function fetchFacultyListData() {
+    const xmlhttp = new XMLHttpRequest();
+    xmlhttp.onload = function () {
+        const abArr = JSON.parse(this.responseText);
+
+        const faculty_list = document.getElementById("faculty_list");
+
+        faculty_list.innerHTML = '';
+
+        abArr.forEach(function (item) {
+            const facultyListItem = document.createElement("ul");
+            facultyListItem.innerHTML = `
+            <li>
+            <a href = "/html/Faculty.html?name=${encodeURIComponent(item.name)}" ><p class = "medium font">${item.name}</p></a>
+            </li>
+            `;
+            faculty_list.appendChild(facultyListItem);
+        });
+    };
+    xmlhttp.open("GET", "/static/json/FacultyLinkList.json", true);
+    xmlhttp.send();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetchFacultyListData();
+});
