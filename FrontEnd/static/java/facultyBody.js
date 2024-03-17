2734c863eee5def6e878578050812423d48372fb
Jude <judemaggitti123@gmail.com>
2024-03-17 11:50:06 -0400

    Implemenmted the new categorys json and started creating not found pages for various webpages

--- FrontEnd/static/java/facultyBody.js
+++ FrontEnd/static/java/facultyBody.js
@@ -2,20 +2,24 @@ function body() {
     const body = document.getElementById("body");
 
     body.innerHTML = `
-    <ul>
-    <li>Authors<ul>
-            <ul id="authorList"></ul>
-        </ul>
-    </li>
-    <li>Abstract<ul>
-            <li><p id="abstract"></p></li>
-        </ul>
-    </li>
-    <li>Citations<ul>
-            <li><p id="citationsList"></p></li>
-        </ul>
-    </li>                                       
-</ul>`;
+                <ul>
+                    <li>Office<ul>
+                            <li><p id="Office"></p></li>
+                        </ul>
+                    </li>
+                    <li>Email<ul>
+                            <li><p id="Email"></p></li>
+                        </ul>
+                    </li>
+                    <li>Department<ul>
+                            <li><p id="Department"></p></li>
+                        </ul>
+                    </li>
+                    <li>Citations<ul>
+                        <ul id="ArticleList"></ul>
+                        </ul>
+                    </li>
+                </ul>`;
 }
 document.addEventListener("DOMContentLoaded", function () {
     body();
