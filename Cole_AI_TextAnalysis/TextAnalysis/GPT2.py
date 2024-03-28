87a1be9bbdce3f01aa33ee1c3a13c6546152fe1d
Cole Barbes <colebarbes@debian.desktop>
2024-03-28 00:08:36 -0400

    Added the beginning of the Taxonomy Framework

--- Cole_AI_TextAnalysis/TextAnalysis/GPT2.py
+++
@@ -1,5 +0,0 @@
-from transformers import pipeline, set_seed
-
-generator = pipeline('text-generation', model='gpt2')
-set_seed(42)
-generator("Hello, I'm a language model, ", max_length=30, num_return_sequences=5)
