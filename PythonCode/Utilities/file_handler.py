1db1e30d9bab7b9c35135428d1f77e97f0756bdb
Spencer Presley <spencerpresley96@gmail.com>
2024-03-31 19:55:08 +0000

    Introduced strategy pattern and implemented strategy for 'author' attribute'

--- PythonCode/Utilities/file_handler.py
+++ PythonCode/Utilities/file_handler.py
@@ -6,11 +6,12 @@ class FileHandler:
     def __init__(self, utils):
         self.utils = utils
 
-    def construct_categories(self, directory_path, category_processor):
+    @staticmethod
+    def construct_categories(*, directory_path, category_processor):
         for filename in os.listdir(directory_path):
             file_path = os.path.join(directory_path, filename)
             # ensure it's a file
-            if self.check_file_status(file_path):
+            if FileHandler.check_file_status(file_path=file_path):
                 with open(file_path, "r") as current_file:
                     category_processor.category_finder(current_file, file_path)
             else:
@@ -18,11 +19,14 @@ class FileHandler:
                     f"Warning: Could not verify file at: {file_path} as a file. Continuing to next file."
                 )
 
-    def check_file_status(self, file_path) -> bool:
+    @staticmethod
+    def check_file_status(*, file_path) -> bool:
         if os.path.isfile(file_path):
             return True
         return False
     
-    def save_cat_dict(self, file_path: str, cat_dict: dict):
+    
+    @staticmethod
+    def save_cat_dict(file_path: str, cat_dict: dict):
         with open(file_path, "wb") as f:
             pickle.dump(cat_dict, f)
