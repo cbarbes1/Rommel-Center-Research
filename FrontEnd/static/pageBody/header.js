6020c8e14425916dec036dba7ea38a0c5ae0c826
Jude <judemaggitti123@gmail.com>
2024-03-31 20:58:05 -0400

    started creating templated site version

---
+++ FrontEnd/static/pageBody/header.js
@@ -0,0 +1,19 @@
+function header() {
+    const header = document.getElementById("nav");
+
+    header.innerHTML = `
+    <div class="flavor-header">
+        <header class="site-header">
+            <h1>Salsibury Research</h1>
+            <nav class="navigation">
+                <a href="#" class="nav-link" onclick="loadPage('indexBody')">Home</a>
+                <a href="#" class="nav-link" onclick="loadPage('TopicSearch')">Topic A-Z</a>
+                <a href="#" class="nav-link" onclick="loadPage('FacultyListSearch')">Faculty Contact</a>
+                <a href="#" class="nav-link" onclick="loadPage('ArticleListSearch')">Articles A-Z</a>
+            </nav>
+        </header>
+    </div>`;
+}
+document.addEventListener("DOMContentLoaded", function () {
+header();
+});
